37c9229e6aab20dcb0fb6e8c456f2368fe10cbd4
Shahwar Spall <shevy.spall@gmail.com>
2019-07-19 19:24:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,13 +15,5 @@ def assign_rooms(names)
   end
   room
 end
-def printer(attendees)
-  attendee_badges = batch_badge_creator(attendees)
-  attendee_rooms = assign_rooms(attendees)
-  attendee_badges.each do |badge|
-    puts badge
-  end
-  attendee_rooms.each do |room_message|
-    puts room_message
-  end
+
 end 
